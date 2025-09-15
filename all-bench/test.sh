d6eae4a7e1e40058b44b60bbde880356a4d3851b
shaohao99 <shaohao@orcd-login001.mit.edu>
2025-09-15 15:41:14 -0400

    update

--- all-bench/test.sh
+++
@@ -1,8 +0,0 @@
-echo $1
-echo $2
-
-if [ -f "run-all.sh" ]; then
-  echo "The file exists and is a regular file."
-else
-  echo "The file does not exist or is not a regular file."
-fi
--- mpi-calc-pi/src/env.sh
+++
@@ -1,4 +0,0 @@
-module load deprecated-modules
-module use /orcd/software/community/001/old_modulefiles/rocky8
-module load openmpi/4.1.4-pmi-ucx-x86_64
-
